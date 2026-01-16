1b55ed7ff97a6a6bed17f85234f7cf4a67718c75
tamim <t.a.fahim24@gmail.com>
2026-01-16 23:28:48 +0600

    add search option

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,6 +4,8 @@ from .models import Categories, Blogs
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
+    list_display = ('title','category','author','status','is_featured')
+    search_fields = ('id','title','category__category_name','status','author__username')
 
 admin.site.register(Categories)
 admin.site.register(Blogs,BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -15,8 +15,8 @@ class Categories(models.Model):
         return self.category_name
     
 STATUS_CHOICES = (
-    (0, "Draft"),
-    (1, "Published")
+    ('Draft', "Draft"),
+    ('Published', "Published")
 )
 
 class Blogs(models.Model):
@@ -27,7 +27,7 @@ class Blogs(models.Model):
     featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
     short_description = models.TextField(max_length=500, blank=True)
     blog_body = models.TextField(max_length=2000)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=0)
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
