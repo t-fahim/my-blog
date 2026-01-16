566144650a57d2fee6f9d28fe5bb72679072c938
tamim <t.a.fahim24@gmail.com>
2026-01-16 20:02:23 +0600

    add model categories

---
+++ blogs/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+    
+
